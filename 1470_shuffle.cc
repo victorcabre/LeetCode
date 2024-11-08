87bf515cafd9a45d1ec5d42eb1e9c7f5002db320
victorcabre <victorcabreg@gmail.com>
2024-11-09 00:32:04 +0100

    added 1470

---
+++ 1470_shuffle.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> sol;
+        for (int i = 0; i < n; ++i) {
+            sol.push_back(nums[i]);
+            sol.push_back(nums[n + i]);
+        }
+
+        return sol;
+    }
+};
+
+int main() {
+    vector<int> nums = {1,2,3,4,5,6};
+    int n = 3;
+
+    Solution s;
+    // cout << s.shuffle(nums, n) << endl;
+    vector<int> sol = s.shuffle(nums, n);
+    for (int num: sol) {
+        cout << num << ' ';
+    }
+}
