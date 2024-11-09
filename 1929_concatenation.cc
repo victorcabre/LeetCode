774688a187ee94c4e89488a619d88834eb517c4f
victorcabre <victorcabreg@gmail.com>
2024-11-09 10:35:55 +0100

    added 1929

---
+++ 1929_concatenation.cc
@@ -0,0 +1,13 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int len = nums.size();
+        for (int i = 0; i < len; ++i) {
+            nums.push_back(nums[i]);
+        }
+        return nums;
+    }
+};
